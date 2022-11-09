2acb10081f7d3a5297a314ed5336a54c41f60150
nidhi <inidhi99@gmail.com>
2022-11-09 17:47:00 -0500

    added user, comment, post, reaction assication

--- models/index.js
+++ models/index.js
@@ -1,25 +1,61 @@
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
-const Reaction = require('./Reaction');
-const Notification = require('./Notification');
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+const Reaction = require("./Reaction");
+const Notification = require("./Notification");
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+Post.hasMany(Reaction, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+Reaction.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+Reaction.belongsTo(Post, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE",
+  });
+  
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+  });
 
 
-
-
-
-
-
-
-
-
+Reaction.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+  });
 
 
 
 module.exports = {
-    User,
-    Post, 
-    Comment,
-    Reaction, 
-    Notification,
-  };
+  User,
+  Post,
+  Comment,
+  Reaction,
+  Notification,
+};
