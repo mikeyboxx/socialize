147efdb26bb5dcf641a14b79aa44b0075c28d0fb
nidhi <inidhi99@gmail.com>
2022-11-09 17:41:00 -0500

    exported all the models in index.js

---
+++ models/index.js
@@ -0,0 +1,25 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const Reaction = require('./Reaction');
+const Notification = require('./Notification');
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    User,
+    Post, 
+    Comment,
+    Reaction, 
+    Notification,
+  };
