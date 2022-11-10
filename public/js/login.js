4373f1fb1128e95ee23278e68d518e13562cf3e2
Malvina Hasa <hasamalvina24@gmail.com>
2022-11-10 12:16:52 -0500

    Added conditional statement to fetch user login using POST method

--- public/js/login.js
+++ public/js/login.js
@@ -5,5 +5,19 @@ const loginFormHandler = async event => {
     const password = document.querySelector('#password-login').value.trim();
     console.log(email, password);
 
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+          method: 'POST',
+          body: JSON.stringify({ email, password }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+          document.location.replace('/');
+        } else {
+          alert('Failed to log in.');
+        }
+      }
+    };
 
   document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
