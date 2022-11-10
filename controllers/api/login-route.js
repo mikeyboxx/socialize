ba72fbe7b95ed7be441e9fa93c610435064d7e96
Malvina Hasa <hasamalvina24@gmail.com>
2022-11-10 14:52:17 -0500

    Added get rout for login

---
+++ controllers/api/login-route.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+// Login route
+router.get('/', (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  // Otherwise, render the 'login' template
+  res.render('login', {
+    loggedIn: req.session.loggedIn,
+    title: 'Socialize',
+  });
+});
+
+module.exports = router;
