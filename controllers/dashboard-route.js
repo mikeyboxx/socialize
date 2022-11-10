eb4061bd0bad75f8cbe125fd2b5a944e6b08518b
Jules Joseph <julesj93@gmail.com>
2022-11-10 14:46:52 -0500

    added dashboard route

--- controllers/dashboard-route.js
+++ controllers/dashboard-route.js
@@ -1,38 +1,15 @@
-const router = require('express').Router();
-const {User, Post} = require('../models');
+const express = require('express')
+const router = express.Router()
+
+// middleware that is specific to this router
+router.use((req, res, next) => {
+  console.log('Time: ', Date.now())
+  next()
+})
+// define the home page route
+router.get('/', (req, res) => {
+  res.send('Dashboard home page')
+})
 
 
-router.get('/', async (req, res) => {
-  if (!req.session.loggedIn) {
-    res.redirect('/login');
-    return;
-  }
-  
-
-  // try {
-  //   const posts = await Post.findAll({
-  //     include: [{ 
-  //       model: User,
-  //       attributes: ['username']
-  //     }],
-  //     order: [['createdAt', 'DESC']],
-  //     attributes: ['id', 'title', 'content', 'createdAt']
-  //   });
-
-  //   // res.json(posts);
-
-  //   res.render('homepage', {
-  //     loggedIn: req.session.loggedIn,  
-  //     title: 'The Tech Blog',
-  //     posts: posts.map(post => post.get(({ plain: true })))
-  //   });
-
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
-});
-
-
-
-
-module.exports = router;
+module.exports = router
