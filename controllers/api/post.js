c0ef6bbefd53f54c61e52085d610d6e48ce26e04
nidhi <inidhi99@gmail.com>
2022-11-10 11:55:12 -0500

    added api route - post route.js

--- controllers/api/post.js
+++ controllers/api/post.js
@@ -1,16 +1,15 @@
 const router = require("express").Router();
-const { Reaction } = require("../../models");
+const { Post } = require("../../models");
 const withAuth = require("../../middleware/auth");
 
 //route to add a reaction
 router.post("/", withAuth, async (req, res) => {
   try {
-    const dbReactionData = await Reaction.create({
-      type: req.body.type,
-      post_id: req.body.post_id,
+    const dbPostData = await Post.create({
+      content: req.body.content,
       user_id: req.session.user_id,
     });
-    res.status(200).json(dbReactionData);
+    res.status(200).json(dbPostData);
   } catch (err) {
     console.log(err);
     res.status(400).json(err);
