c95aa327b47485d4cc1c52526019a6c2117bd63c
Jules Joseph <julesj93@gmail.com>
2022-11-10 11:42:27 -0500

    added homepage route starter code

--- controllers/homepage-route.js
+++ controllers/homepage-route.js
@@ -1,32 +1,32 @@
 const router = require('express').Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const {User, Post} = require('../models');
 
-router.get('/', withAuth, async (req, res) => {
+
+router.get('/', async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    const posts = await Post.findAll({
+      include: [{ 
+        model: User,
+        attributes: ['username']
+      }],
+      order: [['createdAt', 'DESC']],
+      attributes: ['id', 'title', 'content', 'createdAt']
     });
 
-    const users = userData.map((project) => project.get({ plain: true }));
+    // res.json(posts);
 
     res.render('homepage', {
-      users,
-      logged_in: req.session.logged_in,
+      loggedIn: req.session.loggedIn,  
+      title: 'The Tech Blog',
+      posts: posts.map(post => post.get(({ plain: true })))
     });
+
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
 
-  res.render('login');
-});
+
 
 module.exports = router;
