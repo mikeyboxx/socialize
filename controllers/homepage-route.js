9e6d675be53ebd0ecd48018487c440a6f8a87eed
Michael Nabatov <happyanddebtfree@gmail.com>
2022-11-11 10:09:48 -0500

    Tweaekd the queries in homepage-routes.js

--- controllers/homepage-route.js
+++ controllers/homepage-route.js
@@ -57,36 +57,4 @@ router.get('/', async (req, res) => {
 });
 
 
-
-
-// })
-
-
-// router.get('/', async (req, res) => {
-//   try {
-//     const posts = await Post.findAll({
-//       include: [{
-//         model: User,Comment,Notification,
-//         attributes: ['username']
-//       }],
-//       order: [['createdAt', 'DESC']],
-//       attributes: ['id', 'title', 'content', 'createdAt']
-//     });
-
-//     // res.json(posts);
-
-//     res.render('homepage', {
-//       loggedIn: req.session.loggedIn,
-//       title: 'Socialize',
-//       posts: posts.map(post => post.get(({ plain: true })))
-//     });
-
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-
-
-
 module.exports = router;
