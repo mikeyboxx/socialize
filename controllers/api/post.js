2a99d87a2209375835740a8b9252b8482fa17915
Jules Joseph <julesj93@gmail.com>
2022-11-11 10:20:41 -0500

    added route to create a post may need some altering

--- controllers/api/post.js
+++ controllers/api/post.js
@@ -2,18 +2,18 @@ const router = require("express").Router();
 const { Post } = require("../../models");
 const withAuth = require("../../middleware/auth");
 
-//route to add a post
-router.post("/", withAuth, async (req, res) => {
-  try {
-    const dbPostData = await Post.create({
-      contents: req.body.contents,
-      user_id: req.session.user_id,
-    });
-    res.status(200).json(dbPostData);
-  } catch (err) {
-    console.log(err);
-    res.status(400).json(err);
-  }
+// Create a post
+router.post("/", withAuth, (req, res) => {
+  console.log("creating");
+  Post.create({
+          title: req.body.title,
+          content: req.body.post_content,
+          user_id: req.session.user_id
+      })
+      .then((dbPostData) => res.json(dbPostData))
+      .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+      });
 });
-
 module.exports = router;
