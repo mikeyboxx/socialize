b50db078b6d6549f61639073e77b477879bd8ebd
nidhi <inidhi99@gmail.com>
2022-11-12 16:56:01 -0500

    updated notification

--- public/js/notification.js
+++ public/js/notification.js
@@ -1,3 +1,8 @@
+<div class="notification">
+  <button class="delete"></button>
+  Lorem ipsum
+</div>
+
 document.addEventListener('DOMContentLoaded', () => {
     (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
       const $notification = $delete.parentNode;
