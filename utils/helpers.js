8f6ed7be81da158e0385c965d3ea9f3ad8205a0e
Michael Nabatov <happyanddebtfree@gmail.com>
2022-11-12 22:47:33 -0500

    Fixed the notifications handlebars file, to properly render the notifications list, and update the database with read_flag =true

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,5 +4,8 @@ module.exports = {
   formatDate: (date) => {
     return moment(date).format('LLLL');
     // return date.toLocaleString();
+  },
+  isEqual: function (v1, v2) {    
+    return v1 === v2;
   }
 };
