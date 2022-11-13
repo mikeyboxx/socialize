0538f3696bf09577ee96095213b6313bf9b26f20
Michael Nabatov <happyanddebtfree@gmail.com>
2022-11-13 14:06:11 -0500

    Display locale date, iunstead of UTC date.  Fix seeds file

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,8 +2,7 @@ const moment = require('moment');
 
 module.exports = {
   formatDate: (date) => {
-    return moment(date).format('LLLL');
-    // return date.toLocaleString();
+    return moment(moment(moment.parseZone(date).local().format())).format('LLLL');
   },
   isEqual: function (v1, v2) {    
     return v1 === v2;
