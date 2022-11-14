d39ffc37ec4b4da649b220d982f2170387f98c1f
Kathy Shurr <kathyshurr@kathys-mbp.myfiosgateway.com>
2022-11-14 12:15:14 -0500

    merge

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,14 +1,14 @@
-const newCommentFormHandler = async (loggedIn, idx, event) => {
+const newCommentFormHandler = async (loggedIn, idx, event) => {   //function for handling new comments
     event.stopPropagation(event);
 
     if (!loggedIn) return;
 
-    const contents = document.querySelector(`#newcomment-text${idx}`).value.trim();
-    const postId = document.querySelector(`.comment-id${idx}`).innerText
+    const contents = document.querySelector(`#newcomment-text${idx}`).value.trim();  //takes text entered into text field
+    const postId = document.querySelector(`.comment-id${idx}`).innerText  //pulls post-id hiddent in invisible element on each modal
 
     if (contents && postId) {
       try{
-        let response = await fetch('/api/comments', {
+        let response = await fetch('/api/comments', {   //makes post request to comments api route
           method: 'POST',
           body: JSON.stringify({contents, postId}),
           headers: { 'Content-Type': 'application/json' },
@@ -27,7 +27,7 @@ const newCommentFormHandler = async (loggedIn, idx, event) => {
     }
   };
 
-const submitBtn = document.querySelectorAll('.submit');
+const submitBtn = document.querySelectorAll('.submit'); //adds event listener to submit button of all post modals.
     submitBtn.forEach(function(el, idx) {
-    el.addEventListener('click', newCommentFormHandler.bind(this, $(el).attr('loggedIn'), idx));
+    el.addEventListener('click', newCommentFormHandler.bind(this, $(el).attr('loggedIn'), idx)); 
 });
