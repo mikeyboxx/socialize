15fcc7483a66fb6b5cae2e8fc02228b8c76db682
nidhi <inidhi99@gmail.com>
2022-11-13 21:20:59 -0500

    added notifical modal

--- controllers/notification-route.js
+++ controllers/notification-route.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const sequelize = require("../config/connection");
+const withAuth = require('../middleware/auth');
 const {Notification, User, Comment, Reaction, Post} = require('../models');
 
 // notification route
@@ -26,8 +28,47 @@ router.get('/', async (req, res) => {
         user_id: !req.session.userId ? null : req.session.userId,
         read_flag: false 
       },
-      order: [['createdAt', 'DESC']]
+      order: [['createdAt', 'DESC']], 
+      attributes: {
+        include: [
+          [ 
+            sequelize.literal('(SELECT COUNT(*) FROM comment WHERE comment.post_id = post.id)'), 
+            'totalComments'
+          ],
+          [
+            sequelize.literal(`(SELECT COUNT(*) FROM reaction WHERE reaction.post_id = post.id AND reaction.type = 'like')`), 
+            'totalLikes'
+          ],
+          [
+            sequelize.literal(`(SELECT COUNT(*) FROM reaction WHERE reaction.post_id = post.id AND reaction.type = 'dislike')`), 
+            'totalDislikes'
+          ],
+        ]
+      }
     });
+
+
+
+    const [{"COUNT(*)": totalNbrOfComments}] = await sequelize.query(
+      `SELECT COUNT(*) 
+          FROM post, comment 
+         WHERE post.id = comment.post_id
+           AND post.user_id = ${!req.session.userId ? null : req.session.userId}`,{type: sequelize.QueryTypes.SELECT});
+
+    const [{"COUNT(*)":totalNbrOfLikes}] = await sequelize.query(
+      `SELECT COUNT(*) 
+          FROM post, reaction 
+         WHERE post.id = reaction.post_id
+           AND post.user_id = ${!req.session.userId ? null : req.session.userId}
+
+           AND reaction.type = 'like'`,{type: sequelize.QueryTypes.SELECT});
+           
+    const [{"COUNT(*)":totalNbrOfDisLikes}] = await sequelize.query(
+      `SELECT COUNT(*) 
+          FROM post, reaction 
+         WHERE post.id = reaction.post_id
+           AND post.user_id = ${!req.session.userId ? null : req.session.userId}
+           AND reaction.type = 'dislike'`,{type: sequelize.QueryTypes.SELECT});
 
     const notificationsArr = notifications.map(notification => notification.get(({ plain: true })));
 
@@ -41,6 +82,12 @@ router.get('/', async (req, res) => {
     // res.json(notificationsArr);
 
     res.render('notifications', {
+     user:{
+      totalNbrOfComments,
+      totalNbrOfLikes,
+      totalNbrOfDisLikes
+     },
+     
       notificationCount,
       notifications: notificationsArr,
       loggedIn: req.session.loggedIn
